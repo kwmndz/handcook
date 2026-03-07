df9d38f2ec824bbd1d1fc6ecc5ed817a597dd0b8
kwmndz <kmendez8@binghamton.edu>
2026-03-07 13:46:32 -0500

    hand scene

---
+++ Scripts/hand.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+signal on_hover(name: String)
+signal on_action(name: String)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+		
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pinch"):
+		on_action.emit("test")
+	
+	
+	
+func _on_on_action(name: String):
+	print(name)
+	
+	
