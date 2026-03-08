79012c64a043be041ad2ff642cad69a93834372e
kwmndz <kmendez8@binghamton.edu>
2026-03-08 00:56:16 -0500

    new meshes for lettuce

--- Scripts/ingredients/lettuce.gd
+++ Scripts/ingredients/lettuce.gd
@@ -4,21 +4,31 @@ extends Ingredient
 @export var chopped_visual: MeshInstance3D
 @export var overcooked_visual: MeshInstance3D
 
+@export var default_collision: CollisionShape3D
+@export var chopped_collision: CollisionShape3D
+
 func _ready() -> void:
 	isChoppable = true
 	isCookable = true
 	cook_time = 5.0
-	overcook_time = 0.0  # unused, cook() gos straight to overcooked
+	overcook_time = 0.0  # unused, cook() goes straight to overcooked
 	currentState = State.raw
 
 	default_visual.visible = true
 	chopped_visual.visible = false
 	overcooked_visual.visible = false
 
+	default_collision.disabled = false
+	chopped_collision.disabled = true
+
 func update_visual() -> void:
 	default_visual.visible = currentState == State.raw
 	chopped_visual.visible = currentState == State.chopped
 	overcooked_visual.visible = currentState == State.overcooked
+	
+	if currentState == State.chopped:
+		default_collision.disabled = currentState != State.raw
+		chopped_collision.disabled = currentState == State.raw
 
 func chop() -> void:
 	if currentState != State.raw:
